ef474dcfead972dcc94251dff4b314e3e033e307
Maldo <js.maldonado2@uniandes.edu.co>
2025-05-14 17:18:20 -0500

    fijo

--- probar.py
+++ probar.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # Autores: Juan Sebastián, [tu nombre]
-# ProblemaP3.py
+# ProblemaP3_Optimizado.py
 
 import sys
 from collections import defaultdict, Counter, deque
 
 def eulerian_superstring(kmers):
-    """Superstring mínimo de una componente conexa vía De Bruijn + Eulerian Path."""
+    """Construye superstring mínimo de una componente conexa vía De Bruijn + Eulerian Path."""
     id_map, rev_map = {}, []
     def get_id(s):
         if s not in id_map:
@@ -14,22 +14,23 @@ def eulerian_superstring(kmers):
             rev_map.append(s)
         return id_map[s]
 
-    # 1) Construcción del grafo y grados
-    adj = defaultdict(list)
+    adj = defaultdict(deque)
     out_deg = Counter()
-    in_deg  = Counter()
+    in_deg = Counter()
+
+    # Construcción del grafo dirigido
     for mer in kmers:
-        u, v = get_id(mer[:-1]), get_id(mer[1:])
+        u = get_id(mer[:-1])
+        v = get_id(mer[1:])
         adj[u].append((v, mer[-1]))
         out_deg[u] += 1
-        in_deg[v]  += 1
+        in_deg[v] += 1
 
-    # 2) Orden lexicográfico ASC y deque
+    # Orden lexicográfico de aristas para elegir prefijos mínimos
     for u in adj:
-        adj[u].sort(key=lambda x: x[1])
-        adj[u] = deque(adj[u])
+        adj[u] = deque(sorted(adj[u], key=lambda x: x[1]))
 
-    # 3) Nodo inicial (out-in == 1) o prefijo del primer k-mer
+    # Determinar nodo inicial para camino Euleriano
     start = None
     for u in range(len(rev_map)):
         if out_deg[u] - in_deg[u] == 1:
@@ -38,25 +39,23 @@ def eulerian_superstring(kmers):
     if start is None:
         start = get_id(kmers[0][:-1])
 
-    # 4) Hierholzer
-    node_stack = [start]
+    # Hierholzer: construir camino Euleriano
+    stack = [start]
     char_stack = []
     path = []
-    while node_stack:
-        u = node_stack[-1]
+    while stack:
+        u = stack[-1]
         if adj[u]:
             v, c = adj[u].popleft()
-            node_stack.append(v)
+            stack.append(v)
             char_stack.append(c)
         else:
-            node_stack.pop()
+            stack.pop()
             if char_stack:
                 path.append(char_stack.pop())
 
-    # 5) Reconstrucción
     path.reverse()
     return rev_map[start] + ''.join(path)
-
 
 def overlap(a, b):
     """Máximo solapamiento suffix(a) == prefix(b)."""
@@ -74,10 +73,10 @@ def greedy_merge(strings):
             for j in range(len(strings)):
                 if i != j:
                     ov = overlap(strings[i], strings[j])
-                    if ov > best:
+                    if ov > best or (ov == best and (strings[i] + strings[j][ov:]) < (strings[bi] + strings[bj][best:])):
                         best, bi, bj = ov, i, j
         merged = strings[bi] + strings[bj][best:]
-        # reemplazar el par por la fusión
+        # reemplazar los dos por la cadena fusionada
         if bi < bj:
             strings.pop(bj)
             strings[bi] = merged
@@ -87,54 +86,66 @@ def greedy_merge(strings):
     return strings[0]
 
 def full_superstring(kmers):
-    """Detecta componentes, obtiene su superstring y luego las fusiona."""
-    # 1) Conectividad simple para componentes (grafo no dirigido)
+    """Reconstruye superstring mínimo para todos los kmers, manejando múltiples componentes."""
+    # Mapa de ids para nodos (subcadenas de largo k-1)
     id_map, rev_map = {}, []
-    def get_id_simple(s):
+    def get_id(s):
         if s not in id_map:
             id_map[s] = len(rev_map)
             rev_map.append(s)
         return id_map[s]
 
-    adj = defaultdict(list)
+    # Grafo no dirigido para detectar componentes
+    undirected = defaultdict(list)
     for mer in kmers:
-        u, v = get_id_simple(mer[:-1]), get_id_simple(mer[1:])
-        adj[u].append(v)
-        adj[v].append(u)
+        u = get_id(mer[:-1])
+        v = get_id(mer[1:])
+        undirected[u].append(v)
+        undirected[v].append(u)
 
-    seen = [False]*len(rev_map)
-    comps = []
+    seen = [False] * len(rev_map)
+    comp_strs = []
+
+    # Para cada componente, decidir Euleriano o greedy según grados
     for u in range(len(rev_map)):
         if not seen[u]:
-            stack, comp = [u], []
+            # recolectar nodos de la componente
+            stack = [u]
+            comp_nodes = set()
+            seen[u] = True
             while stack:
                 x = stack.pop()
-                if seen[x]: continue
-                seen[x] = True
-                comp.append(x)
-                for w in adj[x]:
+                comp_nodes.add(x)
+                for w in undirected[x]:
                     if not seen[w]:
+                        seen[w] = True
                         stack.append(w)
-            comps.append(comp)
+            # filtrar kmers de esta componente
+            sub = [mer for mer in kmers if get_id(mer[:-1]) in comp_nodes]
+            # calcular grados para saber si hay camino Euleriano
+            out_deg = Counter()
+            in_deg = Counter()
+            for mer in sub:
+                out_deg[mer[:-1]] += 1
+                in_deg[mer[1:]] += 1
+            diffs = [out_deg[p] - in_deg[p] for p in set(out_deg) | set(in_deg)]
+            # Euleriano si |diff|<=1 y a lo sumo un +1 y un -1
+            if diffs.count(1) <= 1 and diffs.count(-1) <= 1 and all(abs(d) <= 1 for d in diffs):
+                comp_strs.append(eulerian_superstring(sub))
+            else:
+                comp_strs.append(greedy_merge(sub))
 
-    # 2) Superstring por componente
-    comp_strs = []
-    for comp in comps:
-        comp_set = set(comp)
-        sub = [mer for mer in kmers if get_id_simple(mer[:-1]) in comp_set]
-        comp_strs.append(eulerian_superstring(sub))
-
-    # 3) Fusionar, empezando por los más largos
+    # finalmente, merge de componentes de menor a mayor longitud
     comp_strs.sort(key=len)
     return greedy_merge(comp_strs)
 
 def main():
-    data = sys.stdin.read().strip().split()
+    data = sys.stdin.buffer.read().split()
     t = int(data[0]); idx = 1
     out = []
     for _ in range(t):
         n, k = map(int, data[idx:idx+2]); idx += 2
-        kmers = data[idx:idx+n]; idx += n
+        kmers = [b.decode() for b in data[idx:idx+n]]; idx += n
         out.append(full_superstring(kmers))
     sys.stdout.write("\n".join(out))
 
